a6a855e21d159bb745114f227cb93834e2195752
Sixto Santiago Aisa <santiagoaisa@gmail.com>
2013-07-23 12:43:43 -0500

    negocio

---
+++ farmacia/src/com/zarcillo/domain/Producto.java
@@ -0,0 +1,33 @@
+
+package com.zarcillo.domain;
+
+public class Producto {
+    
+    private String idproducto;
+    private String cnomproducto;
+
+    public Producto() {
+    }
+    
+    
+
+    public String getIdproducto() {
+        return idproducto;
+    }
+
+    public void setIdproducto(String idproducto) {
+        this.idproducto = idproducto;
+    }
+
+    public String getCnomproducto() {
+        return cnomproducto;
+    }
+
+    public void setCnomproducto(String cnomproducto) {
+        this.cnomproducto = cnomproducto;
+    }
+    
+    
+    
+    
+}
