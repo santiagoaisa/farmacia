d865d06b25c41fef5737834685cd9e1aedfcfd46
rguillen <santiagoaisa@gmail.com>
2013-07-31 14:47:08 -0500

    producto

--- farmacia/src/modmantenimiento/ManttoPresentacion.java
+++ farmacia/src/modmantenimiento/ManttoPresentacion.java
@@ -1,13 +1,186 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package modmantenimiento;
 
-/**
- *
- * @author THIAGO
- */
-public class ManttoPresentacion {
+import com.zarcillo.domain.Presentacion;
+import com.zarcillo.domain.Usuario;
+import com.zarcillo.service.ExceptionZarcillo;
+import com.zarcillo.service.PresentacionService;
+import com.zarcillo.service.UsuarioService;
+import javax.naming.NamingException;
+import modmantenimiento.util.ConstraintCamposObligatorios;
+import modmantenimiento.util.CrudListener;
+import modmantenimiento.util.MenuMantenimiento;
+import org.zkoss.zk.ui.Execution;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.HtmlMacroComponent;
+import org.zkoss.zk.ui.Path;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.VariableResolver;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zul.Checkbox;
+import org.zkoss.zul.ListModel;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Messagebox;
+import org.zkoss.zul.Textbox;
+import org.zkoss.zul.Window;
+
+@VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
+public class ManttoPresentacion extends SelectorComposer implements CrudListener{
     
+    private Presentacion presentacion=new Presentacion();
+    private Usuario usuario;  
+    private MenuMantenimiento menuMantto;   
+    
+    
+    @Wire
+    private Window winPresentacion;
+    
+    @Wire
+    private Textbox txtNombre;
+    
+    @Wire
+    private Textbox txtAbrev;
+          
+    @Wire
+    private Checkbox bActivo;
+    
+        
+    @WireVariable
+    UsuarioService usuarioService;
+    
+    @WireVariable
+    PresentacionService presentacionService;
+    
+        
+    private String user_login;
+    final Execution exec= Executions.getCurrent();
+    
+    
+    
+    @Listen("onCreate=window#winPresentacion")
+    public void onCreate() throws NamingException {
+        HtmlMacroComponent macro = (HtmlMacroComponent) Path.getComponent("/winPresentacion/menuMantto");
+         menuMantto =  (MenuMantenimiento) macro.getChildren().get(0);
+         menuMantto.setCrudlistener(this);
+         initComponets();
+     }
+     
+    public void initComponets(){
+        user_login = exec.getUserPrincipal().getName();
+        usuario=usuarioService.buscarPorLogin(user_login);        
+        habilitar(true);
+    }    
+       
+    @Override
+    public void leer() {     
+        presentacion.setCnompresentacion(txtNombre.getText().toUpperCase());
+        presentacion.setCabrev(txtAbrev.getText());
+        presentacion.setIdusuario(usuario);
+        presentacion.setBactivo(bActivo.isChecked());
+    }    
+    
+    @Override
+    public void escribir() {
+        if (presentacion.getIdpresentacion()== null) {
+            limpiar();
+            return;
+        }
+
+        menuMantto.encuentra();
+        quitarConstraint();
+        txtNombre.setText(presentacion.getCnompresentacion());    
+        txtAbrev.setText(presentacion.getCabrev());
+        bActivo.setChecked(presentacion.getBactivo());
+    }
+
+    @Override
+    public void limpiar() {
+        quitarConstraint();
+        habilitar(false);
+        presentacion=new Presentacion();
+        txtNombre.setText("");
+        txtAbrev.setText("");
+        bActivo.setChecked(false);
+        agregarConstraint();
+    }
+
+    @Override
+    public void buscar() {
+        Window winbuscaprod = (Window) Executions.createComponents("/modulos/mantenimiento/util/busquedapresentacion.zul", null, null);
+        winbuscaprod.setAttribute("REST", true);
+        winbuscaprod.doModal();
+        Boolean rest = (Boolean) winbuscaprod.getAttribute("REST");
+        if (rest) {
+            Listbox lstproducto1 = (Listbox) winbuscaprod.getFellow("lstPresentacion");
+            ListModel modelobuscado = lstproducto1.getModel();
+            presentacion = (Presentacion) modelobuscado.getElementAt(lstproducto1.getSelectedIndex());
+            escribir();
+        }
+    }
+
+    @Override
+    public void modificar() {
+        agregarConstraint();
+        if (presentacion.getIdpresentacion() == null) {
+            throw new ExceptionZarcillo("Debe Buscar Presentacion...");
+        }
+    }
+
+    @Override
+    public void grabar() {        
+        presentacionService.registrar(presentacion);
+        Messagebox.show("Registro Satisfactorio");
+    }
+
+    @Override
+    public void actualizar() {
+        presentacionService.actualizar(presentacion);
+        Messagebox.show("Registro Satisfactorio");
+    }
+
+    @Override
+    public void eliminar() {
+        if (presentacion.getIdpresentacion()== null) {
+            throw new ExceptionZarcillo("Debe Buscar Motivo...");
+        }
+
+        int resp = Messagebox.show("Esta Seguro de eliminar el actual Registro", "Mantenimiento...", Messagebox.OK | Messagebox.CANCEL, Messagebox.QUESTION);
+        if (resp == Messagebox.OK) {
+            presentacionService.eliminar(presentacion);
+            presentacion=new Presentacion();
+            Messagebox.show("La presentacion Fue Eliminada", "Presentacion", Messagebox.OK, Messagebox.INFORMATION);
+        }
+    }
+
+    @Override
+    public void imprimir() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void habilitar(boolean enable) {
+        txtNombre.setReadonly(enable);
+        txtAbrev.setReadonly(enable);
+        bActivo.setDisabled(enable);
+    }
+
+    @Override
+    public void validarDatos() {
+        txtNombre.getValue();
+    }
+
+    @Override
+    public void salir() {
+        winPresentacion.onClose();
+    }   
+
+    public void agregarConstraint() {
+        txtNombre.setConstraint(new ConstraintCamposObligatorios());
+    }
+
+    public void quitarConstraint() {
+        txtNombre.setConstraint("");
+    }   
 }
+
--- farmacia/src/modmantenimiento/ManttoProducto.java
+++ farmacia/src/modmantenimiento/ManttoProducto.java
@@ -1,13 +1,308 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package modmantenimiento;
 
-/**
- *
- * @author THIAGO
- */
-public class ManttoProducto {
+import com.zarcillo.domain.Familia;
+import com.zarcillo.domain.Presentacion;
+import com.zarcillo.domain.Producto;
+import com.zarcillo.domain.Sublinea;
+import com.zarcillo.domain.Ubigeo;
+import com.zarcillo.domain.Usuario;
+import com.zarcillo.service.ExceptionZarcillo;
+import com.zarcillo.service.FamiliaService;
+import com.zarcillo.service.PresentacionService;
+import com.zarcillo.service.ProductoService;
+import com.zarcillo.service.SublineaService;
+import com.zarcillo.service.UbigeoService;
+import com.zarcillo.service.UsuarioService;
+import javax.naming.NamingException;
+import modmantenimiento.util.ConstraintCamposObligatorios;
+import modmantenimiento.util.CrudListener;
+import modmantenimiento.util.MenuMantenimiento;
+import org.zkoss.zk.ui.Execution;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.HtmlMacroComponent;
+import org.zkoss.zk.ui.Path;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.VariableResolver;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zul.Checkbox;
+import org.zkoss.zul.Combobox;
+import org.zkoss.zul.ListModel;
+import org.zkoss.zul.ListModelList;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Messagebox;
+import org.zkoss.zul.Textbox;
+import org.zkoss.zul.Window;
+
+@VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
+public class ManttoProducto extends SelectorComposer implements CrudListener{
     
+    private Producto producto=new Producto();
+    private Usuario usuario;    
+    private MenuMantenimiento menuMantto;   
+    
+    private ListModelList modeloSublinea;
+    private ListModelList modeloFamilia;
+    private ListModelList modeloPresentacion;
+    
+    @Wire
+    private Window winProducto;
+    
+    @Wire    
+    private Textbox txtNombre;
+    
+    @Wire
+    private Textbox txtCodigo;
+    
+    @Wire
+    private Textbox txtBarras;
+    
+    @Wire
+    private Combobox cboSublinea;
+    
+    @Wire
+    private Combobox cboFamilia;
+    
+    @Wire
+    private Combobox cboPresentacion;
+    
+       
+    @Wire
+    private Checkbox bInafecto;
+        
+    @WireVariable
+    UsuarioService usuarioService;
+    
+    @WireVariable
+    ProductoService productoService;
+    
+    @WireVariable
+    SublineaService sublineaService;
+    
+    @WireVariable
+    FamiliaService familiaService;
+    
+    @WireVariable
+    PresentacionService presentacionService;
+    
+    private String user_login;
+    final Execution exec= Executions.getCurrent();
+    
+    
+    
+    @Listen("onCreate=window#winProducto")
+    public void onCreate() throws NamingException {
+        HtmlMacroComponent macro = (HtmlMacroComponent) Path.getComponent("/winProducto/menuMantto");
+         menuMantto =  (MenuMantenimiento) macro.getChildren().get(0);
+         menuMantto.setCrudlistener(this);
+         initComponets();
+     }
+     
+    @Listen("onOK = #txtCodigo")
+    public void onFocoNombre(Event event) {
+        txtNombre.select();
+        buscarProducto();
+    }
+    
+         
+    @Listen("onOK = #txtNombre")
+    public void onFocoSublinea(Event event) {
+        cboSublinea.select();
+    }
+    
+    @Listen("onOK = #cboSublinea")
+    public void onFocoFamilia(Event event) {
+        cboFamilia.focus();
+    }
+    
+    @Listen("onOK = #cboFamilia")    
+    public void onFocoPresentacion(Event event) {
+        cboPresentacion.focus();
+    }
+    
+    @Listen("onOK = #cboPresentacion")
+    public void onFocoBarras(Event event) {
+        txtBarras.focus();
+    }
+    
+         
+    public void initComponets(){
+        user_login = exec.getUserPrincipal().getName();
+        usuario=usuarioService.buscarPorLogin(user_login);
+        modeloSublinea=new ListModelList(sublineaService.listaGeneral());
+        cboSublinea.setModel(modeloSublinea);
+        modeloFamilia=new ListModelList(familiaService.listaGeneral());
+        cboFamilia.setModel(modeloFamilia);
+        modeloPresentacion=new ListModelList(presentacionService.listaGeneral());
+        cboPresentacion.setModel(modeloPresentacion);    
+        habilitar(true);
+    }    
+    
+    public void buscarProducto() {        
+        if (txtCodigo.getValue().isEmpty()) {
+            try {
+                Window winbuscaprod = (Window) Executions.createComponents("/modulos/mantenimiento/util/busquedaproducto.zul", null, null);
+                winbuscaprod.setAttribute("REST", true);
+                winbuscaprod.doModal();
+                Boolean rest = (Boolean) winbuscaprod.getAttribute("REST");
+                if (rest) {
+                    Listbox lstproducto1 = (Listbox) winbuscaprod.getFellow("lstproducto");
+                    ListModel modelobuscado = lstproducto1.getModel();
+                    producto = (Producto) modelobuscado.getElementAt(lstproducto1.getSelectedIndex());
+
+                    escribir();
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            //producto = productoService.buscarProducto(txtCodigo.getValue());
+            escribir();
+        }
+    }     
+    
+    public void buscarCodigoBarras(){
+        //producto = productoService.buscarCodigoBarra(txtBarras.getValue());
+            escribir();
+    }
+
+    @Override
+    public void leer() {     
+        Sublinea sublinea=(Sublinea) modeloSublinea.getElementAt(cboSublinea.getSelectedIndex());
+        Familia familia=(Familia) modeloFamilia.getElementAt(cboFamilia.getSelectedIndex());
+        Presentacion presentacion=(Presentacion) modeloPresentacion.getElementAt(cboPresentacion.getSelectedIndex());
+        producto.setIdproducto(txtCodigo.getText());
+        producto.setCnomproducto(txtNombre.getText().toUpperCase());
+        producto.setIdsublinea(sublinea);
+        producto.setIdfamilia(familia);
+        producto.setIdpresentacion(presentacion);
+        producto.setIdusuario(usuario);
+        producto.setCcodigobarra(txtBarras.getText());
+        producto.setBinafecto(bInafecto.isChecked());
+    }
+    
+    @Override
+    public void escribir() {
+        if (producto.getIdproducto() == null) {
+            limpiar();
+            return;
+        }
+
+        menuMantto.encuentra();
+        quitarConstraint();
+        txtCodigo.setText(producto.getIdproducto());
+        txtNombre.setText(producto.getCnomproducto());
+        cboSublinea.setSelectedIndex(modeloSublinea.indexOf(producto.getIdsublinea()));
+        cboFamilia.setSelectedIndex(modeloFamilia.indexOf(producto.getIdfamilia()));
+        cboPresentacion.setSelectedIndex(modeloPresentacion.indexOf(producto.getIdpresentacion()));
+        txtBarras.setText(producto.getCcodigobarra());        
+        bInafecto.setChecked(producto.getBinafecto());
+    }
+
+    @Override
+    public void limpiar() {
+        quitarConstraint();
+        habilitar(false);
+        producto=new Producto();
+        txtCodigo.setText("");
+        txtNombre.setText("");
+        txtBarras.setText("");
+        cboSublinea.setSelectedIndex(-1);
+        cboSublinea.setText("");
+        cboFamilia.setSelectedIndex(-1);
+        cboFamilia.setText("");
+        cboPresentacion.setSelectedIndex(-1);
+        cboPresentacion.setText("");
+        agregarConstraint();
+    }
+
+    @Override
+    public void buscar() {
+        limpiar();
+        txtCodigo.setDisabled(false);
+        txtCodigo.focus();
+    }
+
+    @Override
+    public void modificar() {
+        agregarConstraint();
+        if (producto.getIdproducto() == null) {
+            throw new ExceptionZarcillo("Debe Buscar Producto...");
+        }
+    }
+
+    @Override
+    public void grabar() {        
+        productoService.registrar(producto);
+        Messagebox.show("Registro Satisfactorio");
+    }
+
+    @Override
+    public void actualizar() {
+        productoService.actualizar(producto);
+        Messagebox.show("Registro Satisfactorio");
+    }
+
+    @Override
+    public void eliminar() {
+        if (producto.getIdproducto() == null) {
+            throw new ExceptionZarcillo("Debe Buscar Producto...");
+        }
+
+        int resp = Messagebox.show("Esta Seguro de eliminar el actual Registro", "Mantenimiento...", Messagebox.OK | Messagebox.CANCEL, Messagebox.QUESTION);
+        if (resp == Messagebox.OK) {
+            productoService.eliminar(producto);
+            producto = new Producto();
+            Messagebox.show("El Producto Fue Eliminado", "Producto", Messagebox.OK, Messagebox.INFORMATION);
+        }
+    }
+
+    @Override
+    public void imprimir() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void habilitar(boolean enable) {
+        txtNombre.setReadonly(enable);
+        txtCodigo.setReadonly(enable);
+        txtBarras.setReadonly(enable);
+        bInafecto.setDisabled(enable);
+        cboFamilia.setDisabled(enable);
+        cboPresentacion.setDisabled(enable);
+        cboSublinea.setDisabled(enable);
+    }
+
+    @Override
+    public void validarDatos() {
+        cboFamilia.getValue();
+        cboPresentacion.getValue();
+        cboSublinea.getValue();
+        txtNombre.getValue();
+        txtCodigo.getValue();
+    }
+
+    @Override
+    public void salir() {
+        winProducto.onClose();
+    }   
+
+    public void agregarConstraint() {
+        txtCodigo.setConstraint(new ConstraintCamposObligatorios());
+        txtNombre.setConstraint(new ConstraintCamposObligatorios());
+        cboPresentacion.setConstraint(new ConstraintCamposObligatorios());
+        cboSublinea.setConstraint(new ConstraintCamposObligatorios());
+        cboFamilia.setConstraint(new ConstraintCamposObligatorios());
+    }
+
+    public void quitarConstraint() {
+        txtCodigo.setConstraint("");
+        txtNombre.setConstraint("");
+        cboFamilia.setConstraint("");
+        cboPresentacion.setConstraint("");
+        cboSublinea.setConstraint("");
+    }   
 }
+
