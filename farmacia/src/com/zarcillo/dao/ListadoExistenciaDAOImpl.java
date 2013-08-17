cbba800c5f71c8603eb3db81ab00276f83430fb3
Sixto Santiago Aisa <santiagoaisa@gmail.com>
2013-08-17 12:49:00 -0500

    listadodao

--- farmacia/src/com/zarcillo/dao/ListadoExistenciaDAOImpl.java
+++ farmacia/src/com/zarcillo/dao/ListadoExistenciaDAOImpl.java
@@ -32,7 +32,7 @@ public class ListadoExistenciaDAOImpl implements ListadoExistenciaDAO {
     public List<InventarioLote> listaPorIdalmacenPorLineasConStock(Integer idalmacen, List<Integer> lista) {
         String sql = "select random() as id,e.idproducto,e.nstock,e.cubicacion,l.nstock as ncanart,l.clote,l.cfecven,e.nstockm,l.nstockm as ncanartm "
                 + "  from existencia e left join lote l on e.idproducto=l.idproducto "
-                + "  and e.idalmacen=l.idalmacen and l.nstock>0,producto p,sublinea s "
+                + "  and e.idalmacen=l.idalmacen and (l.nstock>0 or l.nstockm>0) ,producto p,sublinea s "
                 + "  where e.idproducto=p.idproducto and p.idsublinea=s.idsublinea and e.idalmacen=:idalmacen and s.idlinea in (:lista) and ( e.nstock>0 or e.nstockm>0)  "
                 + "   order by s.idlinea,p.idproducto";
 
