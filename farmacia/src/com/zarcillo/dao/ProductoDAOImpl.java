cac44d0537c720a8089f81985b72e6603bba99ff
Sixto Santiago Aisa <santiagoaisa@gmail.com>
2013-09-03 15:47:56 -0500

    productodao

--- farmacia/src/com/zarcillo/dao/ProductoDAOImpl.java
+++ farmacia/src/com/zarcillo/dao/ProductoDAOImpl.java
@@ -15,33 +15,37 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class ProductoDAOImpl implements ProductoDAO {
-    
+
     @PersistenceContext
     private EntityManager em;
-    
+
     @Override
     public Producto busqueda(String idproducto) {
         return (Producto) em.createNamedQuery("Producto.findByIdproducto").setParameter("idproducto", idproducto).getSingleResult();
     }
-    
+
     @Override
     public Producto busquedaPorCcodigobarra(String ccodigobarra) {
         return (Producto) em.createNamedQuery("Producto.findByCcodigobarra").setParameter("ccodigobarra", ccodigobarra).getSingleResult();
     }
-    
+
     @Override
     public List<Producto> busquedaListaPorDescripcion(String ccriterio) {
-        return em.createNamedQuery("Producto.findByCnomproducto").setParameter("ccriterio", "%".concat(ccriterio.trim().toUpperCase().concat("%"))).getResultList();        
+        return em.createNamedQuery("Producto.findByCnomproducto").setParameter("ccriterio", "%".concat(ccriterio.trim().toUpperCase().concat("%"))).getResultList();
     }
-    
+
     @Override
     public Integer numeroPorIdlinea(Integer idlinea) {
-        String sql = "select count(*)+1 from producto  p,sublinea s "
-                + "where p.idsublinea=s.idsublinea and s.idlinea=:idlinea ";
-        
-        BigInteger idregentrada1 = (BigInteger) em.createNativeQuery(sql).setParameter("idlinea", idlinea).getSingleResult();
-        return idregentrada1.intValue();
-        
-        
+        String sql = "select max(cast(substring(idproducto, 4,6) as Integer))+1 "
+                + " from producto  p,sublinea s "
+                + " where p.idsublinea=s.idsublinea and s.idlinea=:idlinea  ";
+        BigInteger idproducto = new BigInteger("0");
+        try {
+            idproducto = (BigInteger) em.createNativeQuery(sql).setParameter("idlinea", idlinea).getSingleResult();
+        } catch (Exception e) {
+            idproducto = new BigInteger("0");
+
+        }
+        return idproducto.intValue();
     }
 }
